1fd3f236b7507c458bf35c95579d893d8b2f32e9
okparanoid <pyschyl@yandex.ru>
2020-07-06 20:49:27 +0300

    first commit

---
+++ scripts/EditPopup.js
@@ -0,0 +1,14 @@
+class EditPopup extends Popup {
+  constructor(popup) {
+    super();
+    this.popup = popup;
+  }
+
+  open() {
+    super.open();
+    this.popup.querySelector('.popup__button').removeAttribute('disabled');
+    this.popup.querySelector('.popup__button').classList.add('popup__button_is-active');
+    this.popup.querySelector('#user-name-error').textContent = '';
+    this.popup.querySelector('#about-error').textContent = '';
+  }
+}
---
+++ scripts/ImagePopup.js
@@ -0,0 +1,12 @@
+class ImagePopup extends Popup {
+  constructor(popup) {
+    super();
+    this.popup = popup;
+    this.event = event;
+  }
+
+  open(imgUrl) {
+    super.open();
+    this.popup.querySelector('.popup__image').src = imgUrl;
+  }
+}
